4edcc526694b6fd3ce705b5bf96a4cdc36a4b17b
osyoyu <osyoyu@osyoyu.com>
2019-09-30 00:53:12 +0900

    Implement /peephole/{start,finish}

--- lib/rack/peephole.rb
+++ lib/rack/peephole.rb
@@ -1,5 +1,4 @@
 require_relative "peephole/version"
-require_relative "peephole/middleware.rb"
 
 require 'stackprof'
 
@@ -8,12 +7,22 @@ module Rack
     def initialize(app, options = {})
       @app = app
       @stackprof_running = false
+
+      at_exit do
+        if @stackprof_running
+          StackProf.stop
+        end
+      end
     end
 
     def call(env)
       case env['REQUEST_PATH']
       when '/peephole/cpu', '/peephole/wall'
         handle_get_profile(env)
+      when '/peephole/start', '/peephole/start'
+        handle_get_start(env)
+      when '/peephole/finish', '/peephole/finish'
+        handle_get_finish(env)
       else
         @app.call(env)
       end
@@ -28,7 +37,7 @@ module Rack
       return [404, headers, []] unless mode
 
       interval = params['interval'] || 1000
-      raw = params['raw'] || false
+      raw = !!(params['raw']) || false
       profile_time_seconds = params['profile_time_seconds'] || 30
 
       @stackprof_running = true
@@ -56,6 +65,53 @@ module Rack
       return [500, headers, [e]]
     ensure
       @stackprof_running = false
+    end
+
+    # GET /peephole/start
+    def handle_get_start(env)
+      params = Rack::Utils.parse_query(env['QUERY_STRING'])
+      headers = {}
+
+      mode = params['mode']&.to_sym || :cpu
+      interval = params['interval'] || 1000
+      raw = !!(params['raw']) || false
+
+      if @stackprof_running
+        headers['Content-Length'] = "0"
+        return [429, headers, []]
+      end
+
+      @stackprof_running = true
+      StackProf.start(
+        mode: mode,
+        interval: interval,
+        raw: raw,
+      )
+
+      return [200, headers, ["ok"]]
+    end
+
+    # GET /peephole/finish
+    def handle_get_finish(env)
+      params = Rack::Utils.parse_query(env['QUERY_STRING'])
+      headers = {}
+
+      if !@stackprof_running
+        headers['Content-Length'] = "0"
+        return [400, headers, []]
+      end
+
+      StackProf.stop
+      @stackprof_running = false
+
+      results = StackProf.results
+      dump = Marshal.dump(results)
+
+      headers['Content-Length'] = dump.length.to_s
+      headers['Content-Type'] = 'application/octet-stream'
+      headers['Content-Disposition'] = 'attachment; filename="profile.dump"'
+
+      return [200, headers, [dump]]
     end
   end
 end
