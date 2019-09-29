26bd2ec1aab411d4404e4f08a9096047d53b60fc
osyoyu <osyoyu@osyoyu.com>
2019-09-30 00:30:31 +0900

    Organize directory structure

--- lib/rack/peephole.rb
+++ lib/rack/peephole.rb
@@ -1,7 +1,61 @@
 require_relative "peephole/version"
 require_relative "peephole/middleware.rb"
 
+require 'stackprof'
+
 module Rack
-  module Peephole
+  class Peephole
+    def initialize(app, options = {})
+      @app = app
+      @stackprof_running = false
+    end
+
+    def call(env)
+      case env['REQUEST_PATH']
+      when '/peephole/cpu', '/peephole/wall'
+        handle_get_profile(env)
+      else
+        @app.call(env)
+      end
+    end
+
+    # GET /peephole/profile/(cpu|wall)
+    def handle_get_profile(env)
+      params = Rack::Utils.parse_query(env['QUERY_STRING'])
+      headers = {}
+
+      mode = env['REQUEST_PATH'].match(%r{^/peephole/(cpu|wall)/?})[1].to_sym
+      return [404, headers, []] unless mode
+
+      interval = params['interval'] || 1000
+      raw = params['raw'] || false
+      profile_time_seconds = params['profile_time_seconds'] || 30
+
+      @stackprof_running = true
+      StackProf.start(
+        mode: mode,
+        interval: interval,
+        raw: raw,
+      )
+
+      sleep profile_time_seconds
+
+      StackProf.stop
+      @stackprof_running = false
+
+      results = StackProf.results
+      dump = Marshal.dump(results)
+
+      headers['Content-Length'] = dump.length.to_s
+      headers['Content-Type'] = 'application/octet-stream'
+
+      return [200, headers, [dump]]
+    rescue => e
+      STDERR.puts e
+      headers['Content-Length'] = "0"
+      return [500, headers, [e]]
+    ensure
+      @stackprof_running = false
+    end
   end
 end
--- lib/rack/peephole/version.rb
+++ lib/rack/peephole/version.rb
@@ -1,5 +1,5 @@
 module Rack
-  module Peephole
+  class Peephole
     VERSION = "0.1.0"
   end
 end
